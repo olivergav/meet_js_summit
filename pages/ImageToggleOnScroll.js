29008763781ab8779770cc745b6751b2c49bf475
olivergav <oliwier.gawlik0@gmail.com>
2022-09-13 23:04:12 +0200

    image changes color when its on display

--- pages/ImageToggleOnScroll.js
+++ pages/ImageToggleOnScroll.js
@@ -1,13 +1,33 @@
 import ImageChangeOnScroll from "../src/ImageChangeOnScroll";
+import {useEffect, useState} from "react";
 
 function ImageToggleOnScroll(props){
+    const [currentSpeakerId, setCurrentSpeakerId] = useState(0);
+    const [mouseEventCounter, setMouseEventCounter] = useState(0);
+
+    useEffect(() => {
+        window.document.title = `SpeakerID: ${currentSpeakerId}`
+    }, [currentSpeakerId])
+
     return (
         <>
+            <span>
+                Mouse Event Counter: {mouseEventCounter}
+            </span>
+
             {[1124, 187, 823, 1269, 1530].map((speakerId) => (
-                <ImageChangeOnScroll
+                <div
                     key={speakerId}
-                    primaryImage={`./static/speakers/bw/Speaker-${speakerId}.jpg`}
-                    secondaryImage={`./static/speakers/Speaker-${speakerId}.jpg`}/>
+                    onMouseOver={() => {
+                        setCurrentSpeakerId(speakerId);
+                        setMouseEventCounter(mouseEventCounter + 1);
+
+                        // console.log(`On mouse over: ${mouseEventCounter}`);
+                    }}>
+                    <ImageChangeOnScroll
+                        primaryImage={`./static/speakers/bw/Speaker-${speakerId}.jpg`}
+                        secondaryImage={`./static/speakers/Speaker-${speakerId}.jpg`}/>
+                </div>
             ))}
 
         </>
--- src/ImageChangeOnScroll.js
+++ src/ImageChangeOnScroll.js
@@ -1,9 +1,36 @@
 import ImageToggleOnScroll from "../pages/ImageToggleOnScroll";
+import {useEffect, useRef, useState} from "react";
 
-function ImageChangeOnScroll({primaryImage, secondaryImage}){
+function ImageChangeOnScroll({primaryImage, secondaryImage}) {
+    const [inView, setInView] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const imageRef = useRef(null);
+
+    function isInView() {
+        const rect = imageRef.current.getBoundingClientRect();
+        return rect.top >= 0 && rect.bottom <= window.innerHeight;
+    }
+
+    function scrollHandler() {
+        setInView(isInView());
+    }
+
+    useEffect(() => {
+        setIsLoading(false);
+        scrollHandler();
+        window.addEventListener('scroll', scrollHandler);
+
+        return () => {
+            window.removeEventListener('scroll', scrollHandler);
+        }
+    }, []);
+
     return (
         <>
-            <img src={primaryImage} alt=""/>
+            <img src={isLoading ? 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==' // 1x1gif
+                        : inView ? secondaryImage : primaryImage}
+                 ref={imageRef}
+                 alt=""/>
         </>
     )
 }
