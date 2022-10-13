b12e3c6858062bafdd5e99ad5f3e9f01608da626
olivergav <oliwier.gawlik0@gmail.com>
2022-10-13 21:15:29 +0200

    speakers filter and memoization

--- src/Speakers.js
+++ src/Speakers.js
@@ -1,18 +1,43 @@
-import {useCallback, useContext} from "react";
+import {useCallback, useContext, useMemo, useState} from "react";
 import Header from "./Header";
 import Menu from "./Menu";
 import {GlobalContext} from "./GlobalState";
 import SpeakerDetail from "./SpeakerDetail";
 
 function Speakers(){
-    const {isLoading, speakerList, toggleSpeakerFavorite} = useContext(GlobalContext)
+    const {isLoading, speakerList, toggleSpeakerFavorite} = useContext(GlobalContext);
+    const [speakersSaturday, setSpeakersSaturday] = useState(true);
+    const [speakersSunday, setSpeakersSunday] = useState(true);
 
-    if (isLoading) return <div>Loading...</div>
-
-    // eslint-disable-next-line react-hooks/rules-of-hooks
     const heartFavoriteHandler = useCallback(function (speaker) {
         toggleSpeakerFavorite(speaker)
-    }, [])
+    }, []);
+
+    function handleChangeSaturday() {
+        setSpeakersSaturday(!speakersSaturday)
+    }
+
+    function handleChangeSunday() {
+        setSpeakersSunday(!speakersSunday)
+    }
+
+    const filteredSpeakerList =  useMemo(() => {
+        return speakerList
+            .filter(({sat, sun}) => (sat && speakersSaturday) || (sun && speakersSunday))
+            .sort((a, b) => {
+                if (a.firstName < b.firstName) {
+                    return - 1;
+                }
+
+                if (a.firstName > b.firstName) {
+                    return 1;
+                }
+
+                return 0;
+            })
+    }, [speakersSaturday, speakersSunday, speakerList])
+
+    if (isLoading) return <div>Loading...</div>;
 
     return (
         <>
@@ -20,13 +45,43 @@ function Speakers(){
             <Menu />
 
             <div className='container'>
-                {speakerList.map((speaker) => (
-                    <SpeakerDetail
-                        speaker={speaker}
-                        key={speaker.id}
-                        heartFavoriteHandler={heartFavoriteHandler}
-                    />
-                ))}
+                <div className="btn-toolbar  margintopbottom5 checkbox-bigger">
+                    <div className="hide">
+                        <div className="form-check-inline">
+                            <label className="form-check-label">
+                                <input
+                                    type="checkbox"
+                                    className="form-check-input"
+                                    checked={speakersSaturday}
+                                    onChange={handleChangeSaturday}
+                                />
+                                Saturday Speakers
+                            </label>
+                        </div>
+                        <div className="form-check-inline">
+                            <label className="form-check-label">
+                                <input
+                                    type="checkbox"
+                                    className="form-check-input"
+                                    checked={speakersSunday}
+                                    onChange={handleChangeSunday}
+                                />
+                                Sunday Speakers
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="speakers">
+                    {filteredSpeakerList
+                        .map((speaker) => (
+                        <SpeakerDetail
+                            speaker={speaker}
+                            key={speaker.id}
+                            heartFavoriteHandler={heartFavoriteHandler}
+                        />
+                    ))}
+                </div>
             </div>
         </>
     )
